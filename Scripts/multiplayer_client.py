c78e248a0ea6509016dfc7e4b7ebc717ff5d37e5
thepancake1 <theojabalde@gmail.com>
2018-04-01 21:49:18 +0800

    Started work on integrating networking code into game state syncing code

--- Scripts/multiplayer_client.py
+++ Scripts/multiplayer_client.py
@@ -4,51 +4,37 @@ from struct import unpack, pack
 import threading
 import time
 import sys
+import mp
 
 from networking import generic_send_loop, generic_listen_loop
 class Client:
     def __init__(self):
         self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        self.serversocket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         self.host = socket.gethostname()                           
         self.port = 9999       
         self.connected = False
 
     def listen(self, incoming_commands):
-        threading.Thread(target = self.listen_loop, args = [incoming_commands]).start()
+        threading.Thread(target = self.listen_loop, args = []).start()
 
-    def send(self, outgoing_commands):
-        threading.Thread(target = self.send_loop, args = [outgoing_commands]).start()
+    def send(self):
+        threading.Thread(target = self.send_loop, args = []).start()
         
-    def send_loop(self, outgoing_commands):
+    def send_loop(self):
         self.serversocket.connect((self.host, self.port))    
         self.connected = True
     
         while True:
-            for data in outgoing_commands:
+            for data in mp.outgoing_commands:
                 generic_send_loop(data, self.serversocket)
-                outgoing_commands.remove(data)
-                time.sleep(0.1)
-            # time.sleep(0.1)
-    def listen_loop(self, incoming_commands):
+                mp.outgoing_commands.remove(data)
+    def listen_loop(self, mp.incoming_commands):
         serversocket = self.serversocket
         
         size = None 
         data = b''
         while True:
             if self.connected:
-                incoming_commands, data, size = generic_listen_loop(serversocket, incoming_commands, data, size)
+                mp.incoming_commands, data, size = generic_listen_loop(serversocket, mp.incoming_commands, data, size)
 
-if __name__ == "__main__":   
-    incoming_commands = []
-    outgoing_commands = []
-    client_instance = Client()
-    client_instance.listen(incoming_commands)
-    client_instance.send(outgoing_commands)
-
-    count = 0
-    while True:
-        print(incoming_commands)
-        incoming_commands = []
-        outgoing_commands.append("Data from client: {}".format(count))
-        count += 1
-        time.sleep(1)
