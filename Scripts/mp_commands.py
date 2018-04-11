a348a78149e7ec551ceff7a65fc6fca94ef3c9e7
thepancake1 <theojabalde@gmail.com>
2018-04-12 00:28:57 +0800

    Client sync should now work even if no client is present. (For file transfers and such)

--- Scripts/mp_commands.py
+++ Scripts/mp_commands.py
@@ -119,7 +119,6 @@ from server_commands.persistence_commands import save_game
 
 def send_lot_architecture_and_reload(_connection = None):
     # output = sims4.commands.CheatOutput(_connection) 
-    save_game()
     # output("working")
     zone = services.current_zone()
     name = str(hex(zone.id)).replace("0x", "")
