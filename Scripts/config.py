d71f0bf666afcef2ef3868dd852b02e55d2ee87a
thepancake1 <theojabalde@gmail.com>
2018-04-19 22:33:40 +0800

    Tidied config file

--- Scripts/config.py
+++ Scripts/config.py
@@ -1,5 +1,6 @@
-user_directory = "C:/Users/theoj/Documents/Electronic Arts/The Sims 4/Mods/Heuristics/Scripts/"
-# host = "0.tcp.ngrok.io"
-# port = 15594
+#settings for everyone
+user_directory = "C:/Users/Theo/Documents/Electronic Arts/The Sims 4/Mods/Heuristics/Scripts/"
+
+#settings only for the client
 host = "0.tcp.ngrok.io"
-port = 12528
+port = 17376
