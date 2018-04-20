f23f7cead1bcad4fa432f44fac370b93953412ba
thepancake1 <theojabalde@gmail.com>
2018-04-20 17:02:06 +0800

    Revert "Log debug to files"
    This reverts commit 5138ba3904c909b9f3a7ccbdec09f7455ec60353.

--- Scripts/update.py
+++ Scripts/update.py
@@ -1,11 +1,11 @@
 from config import user_directory
 
 def output(filename, string):
-    # return
+    return
     with open("{}{}.txt".format(user_directory, filename), 'a') as output:
         output.write(str(string) + "\n")
 
 def output_irregardelessly(filename, string):
-    # return
+    return
     with open("{}{}.txt".format(user_directory, filename), 'a') as output:
         output.write(str(string) + "\n")
