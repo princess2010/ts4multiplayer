a9f97f5d2a678fc64e6a75c23e8714360b9cdab3
thepancake1 <theojabalde@gmail.com>
2018-04-23 20:59:53 +0800

    Remvoe user directory variable

--- Scripts/config.py
+++ Scripts/config.py
@@ -1,6 +1,3 @@
-#settings for everyone
-user_directory = "C:/Users/Theo/Documents/Electronic Arts/The Sims 4/Mods/Heuristics/Scripts/"
-
 #settings only for the client
 host = "0.tcp.ngrok.io"
-port = 17807
+port = 10632
